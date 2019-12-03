4d31aa2bc30c5efd1563f84be3a8eec31ee547f0
soumya-j <soumyajakati1@gmail.com>
2019-12-03 17:05:48 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {AllCommunityModules} from '@ag-grid-community/all-modules';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'grid-example';
+  columnDefs = [
+        {headerName: 'Make', field: 'make' },
+        {headerName: 'Model', field: 'model' },
+        {headerName: 'Price', field: 'price'}
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+
+    modules = AllCommunityModules;
+}
+
+
+
+
